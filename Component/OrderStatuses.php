3b47122a10aa56406541e9077766dfcedf5c1c03
Vladimir Mihasyuk <v.mihasyuk@mobilunity.com>
2019-03-05 17:02:38 +0200

    Create order statuses component
    (cherry picked from commit abb1b361f7976b6cd181a03a00f1bc230d5c76b2)

---
+++ Component/OrderStatuses.php
@@ -0,0 +1,114 @@
+<?php
+namespace CtiDigital\Configurator\Component;
+
+use CtiDigital\Configurator\Api\ComponentInterface;
+use CtiDigital\Configurator\Api\LoggerInterface;
+use CtiDigital\Configurator\Exception\ComponentException;
+use Magento\Sales\Model\Order\Status;
+use Magento\Sales\Model\Order\StatusFactory;
+use Magento\Sales\Model\ResourceModel\Order\Status as StatusResource;
+use Magento\Sales\Model\ResourceModel\Order\StatusFactory as StatusResourceFactory;
+
+/**
+ * Class OrderStatuses
+ * @package CtiDigital\Configurator\Component
+ */
+class OrderStatuses implements ComponentInterface
+{
+    protected $alias = 'order_statuses';
+    protected $name = 'Order Statuses';
+    protected $description = 'Component to create custom order statuses';
+
+    /**
+     * @var StatusFactory
+     */
+    protected $statusFactory;
+
+    /**
+     * @var StatusResourceFactory
+     */
+    protected $statusResourceFactory;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $log;
+
+    /**
+     * OrderStatuses constructor.
+     * @param StatusFactory $statusFactory
+     * @param StatusResourceFactory $statusResourceFactory
+     * @param LoggerInterface $log
+     */
+    public function __construct(
+        StatusFactory $statusFactory,
+        StatusResourceFactory $statusResourceFactory,
+        LoggerInterface $log
+    ) {
+        $this->statusFactory = $statusFactory;
+        $this->statusResourceFactory = $statusResourceFactory;
+        $this->log = $log;
+    }
+
+    /**
+     * @param $data
+     */
+    public function execute($data = null)
+    {
+        if (isset($data['order_statuses'])) {
+            foreach ($data['order_statuses'] as $statusSet) {
+                try {
+                    $this->createOrderStatuses($statusSet);
+                } catch (ComponentException $e) {
+                    $this->log->logError($e->getMessage());
+                }
+            }
+        }
+    }
+
+    /**
+     * @param $statusSet
+     * @throws \Magento\Framework\Exception\AlreadyExistsException
+     */
+    public function createOrderStatuses($statusSet)
+    {
+        foreach ($statusSet['statuses'] as $statusData) {
+            /** @var StatusResource $statusResource */
+            $statusResource = $this->statusResourceFactory->create();
+            /** @var Status $status */
+            $status = $this->statusFactory->create();
+            $status->setData([
+                'status' => $statusData['code'],
+                'label' => $statusData['name'],
+            ]);
+
+            try {
+                $statusResource->save($status);
+            } catch (ComponentException $e) {
+                $this->log->logError($e->getMessage());
+            }
+
+            $status->assignState($statusSet['state'], false, true);
+
+            $this->log->logInfo(
+                sprintf('Order status %s created', $statusData['name'])
+            );
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlias()
+    {
+        return $this->alias;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
