bbef0d776e2df325e6ef6fdd57acd502b5fec51c
Julien ANQUETIL <julien.anquetil@gmail.com>
2025-05-07 08:33:26 +0200

    Update Processor.php

--- Model/Processor.php
+++ Model/Processor.php
@@ -178,7 +178,7 @@ class Processor
                 $this->state->emulateAreaCode(
                     $areaCode,
                     [$this, 'runComponent'],
-                    [$componentAlias, $componentConfig]
+                    [$componentAlias, $masterConfig]
                 );
 
             }
